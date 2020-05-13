50c2c44ed1838d88c0534686cbfb0144fccc46ff
bokor-git <bogdankorolbk@gmail.com>
2020-05-13 20:08:46 +0300

    Updates

---
+++ precache-manifest.a9d5f08706d67c17f7b628f5b13a1f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0602f693637a7de9e55988b1e3c69e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6a38ca34645788596445",
+    "url": "/portfolio/static/css/main.07a17d7b.chunk.css"
+  },
+  {
+    "revision": "a68cef6106665337cefe",
+    "url": "/portfolio/static/js/2.cd43da79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.cd43da79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a38ca34645788596445",
+    "url": "/portfolio/static/js/main.0b32a3f7.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
