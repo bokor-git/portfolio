4e7d818220e007f82d2d9f91c366ecb38181f6f2
bokor-git <bogdankorolbk@gmail.com>
2020-05-30 14:28:25 +0300

    task part 2 projects block done

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -4,13 +4,23 @@ import styleContainer from "../common/styles/Container.module.css"
 import Project from "./project/Project";
 
 
+const socialNetwork = {
+    name: "iRunner",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquam aspernatur atque cum fugit molestiae nihil repudiandae sed totam ullam! Debitis earum re"
+}
+
+const todoList = {
+    name: "Todolist",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquam aspernatur atque cum fugit molestiae nihil repudiandae sed totam ullam! Debitis earum re"
+}
+
 const Projects = () => {
     return <div className={style.projectsBlock}>
         <div className={`${styleContainer.container} ${style.projectContainer}`}>
             <h2 className={style.title}>Projects</h2>
-            <div className={style.projectBlock} >
-                <Project/>
-                <Project/>
+            <div className={style.projectBlock}>
+                <Project name={socialNetwork.name} description={socialNetwork.description}/>
+                <Project name={todoList.name} description={todoList.description}/>
             </div>
 
         </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import style from "./Project.module.css"
 
 
-const Project = () => {
+type ProjectPropsType = {
+    name:string
+    description:string
+}
+
+const Project = (props:ProjectPropsType) => {
     return <div className={style.project}>
         <div className={style.projectPhoto}>
             <a className={style.checkProject} href="">Check project</a>
         </div>
         <div className={style.projectDescription}>
-            <span className={style.projectName}>Project Name</span>
-            <p>Project description </p>
+            <h4>{props.name}</h4>
+            <p>{props.description}</p>
         </div>
     </div>
 
