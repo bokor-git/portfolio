a33293d8e529cf5591165762d26328ffc204725a
bokor-git <bogdankorolbk@gmail.com>
2020-05-30 12:09:00 +0300

    task part 2 main block done

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -6,11 +6,11 @@ const Main = () => {
     return <div className={style.main}>
         <div className={styleContainer.container}>
             <div className={style.text}>
-                <span>Hi There!</span>
-                <h1>I am<br/> Bohdan Korol.</h1>
+                <p>Hi There!</p>
+                <h1>I am<br/> Bohdan Korol</h1>
                 <p>Front-End Developer</p>
             </div>
-            <div className={style.photo}></div>
+            <div className={style.photo}> </div>
         </div>
     </div>
 }
