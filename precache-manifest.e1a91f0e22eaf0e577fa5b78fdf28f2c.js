9e452077a1793f351d3287f70ad6e0296fe11d28
bokor-git <bogdankorolbk@gmail.com>
2020-05-31 10:26:40 +0300

    Updates

---
+++ precache-manifest.e1a91f0e22eaf0e577fa5b78fdf28f2c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598ae2800da31134b50b6f299b120fef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6442d74fc7e876d94edf",
+    "url": "/portfolio/static/css/main.78f2c4c5.chunk.css"
+  },
+  {
+    "revision": "76fb93221caaf4476eeb",
+    "url": "/portfolio/static/js/2.64623330.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.64623330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6442d74fc7e876d94edf",
+    "url": "/portfolio/static/js/main.53e096ff.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5f02ff6100dcd17dcd16a5b0a809be4a",
+    "url": "/portfolio/static/media/counter.5f02ff61.png"
+  },
+  {
+    "revision": "673cbc79f5aa3dbc774013eba86dc936",
+    "url": "/portfolio/static/media/messenger.673cbc79.png"
+  },
+  {
+    "revision": "b9887307904853bc93c0e6fe57225e29",
+    "url": "/portfolio/static/media/social-network.b9887307.png"
+  },
+  {
+    "revision": "650d1708b25c2082180a9514a35b8077",
+    "url": "/portfolio/static/media/todolist.650d1708.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2c55881af227856add420131972bb18a.js"
+  "/portfolio/precache-manifest.e1a91f0e22eaf0e577fa5b78fdf28f2c.js"
 );
 
 self.addEventListener('message', (event) => {
